7575fa8396dd14c15b637cbdfe20b4f4513010e2
Rodrigo Salvadori <30501456+rsalvadoric@users.noreply.github.com>
2022-08-02 17:32:00 -0300

    criando header e nav bar

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,21 @@
+import styles from './Cardapio.module.scss';
+import { ReactComponent as Logo }  from 'assets/logo.svg';
+
+export default function Cardapio() {
+    return (
+        <main>
+            <nav className={styles.menu}>
+                <Logo />
+                Aluroni
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            </section>
+        </main>
+    )
+}
