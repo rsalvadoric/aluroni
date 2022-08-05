a92886b9cf8cf89e298bbd2dceacb04f09976588
Rodrigo Salvadori <30501456+rsalvadoric@users.noreply.github.com>
2022-08-05 11:51:16 -0300

    adicionando menu de ordenar e filtros

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,12 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo }  from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 export default function Cardapio() {
-    const [busca, setBusca] = useState("")
+    const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
     return (
         <main>
             <nav className={styles.menu}>
@@ -19,6 +22,10 @@ export default function Cardapio() {
             <section className={styles.cardapio}>
                 <h3 className={styles.cardapio__titulo}>Cardápio</h3>
                 <Buscador busca={busca} setBusca={setBusca} />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordenador />
+                </div>
             </section>
         </main>
     )
