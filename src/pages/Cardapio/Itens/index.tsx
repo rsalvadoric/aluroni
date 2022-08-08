1e96c0cf4acf5fae27939bc7481d4c235e312cf3
Rodrigo Salvadori <30501456+rsalvadoric@users.noreply.github.com>
2022-08-08 11:38:05 -0300

    adicionando filtros e ordenação do cardápio

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,58 @@
 import cardapio from './itens.json';
 import Item from './Item';
 import styles from './Itens.module.scss'
+import { useEffect, useState } from 'react';
 
-export default function Itens() {
+interface Props {
+    busca: string;
+    filtro: number | null;
+    ordenador: string;
+}
+
+export default function Itens(props: Props) {
+    const [ lista, setLista ] = useState(cardapio);
+    const { busca, filtro, ordenador } = props;
+
+    function testaBusca(title: string) {
+        const regex = new RegExp(busca, 'i');
+        return regex.test(title);
+    }
+
+    function testaFiltro(id: number) {
+        if(filtro !== null) {
+            return filtro === id;
+        }
+        return true;
+    }
+
+    const ordenar = (novaLista: typeof cardapio) => {
+        switch (ordenador) {
+          case 'porcao':
+            return ordenarPropriedadeCrescente(novaLista, 'size');
+          case 'qtd_pessoas':
+            return ordenarPropriedadeCrescente(novaLista, 'serving');
+          case 'preco':
+            return ordenarPropriedadeCrescente(novaLista, 'price');
+          default:
+            return novaLista;
+        }
+    };
+
+    const ordenarPropriedadeCrescente = (
+        lista: typeof cardapio,
+        propriedade: keyof Pick<typeof cardapio[0], 'size' | 'serving' | 'price'>
+      ) => {
+        return lista.sort((a, b) => (a[propriedade] > b[propriedade] ? 1 : -1));
+      };
+
+    useEffect(() => {
+        const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+        setLista(ordenar(novaLista));
+    }, [busca, filtro, ordenador]);
+
     return (
         <div className={styles.itens}>
-            {cardapio.map(item => (
+            {lista.map(item => (
                 <Item key={item.id} {...item} />
             ))}
         </div>
